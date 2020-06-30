8c20d886b31088958363871ea7cc045e1479fb90
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-30 18:04:57 -0300

    :label: configure dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,61 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useState, FormEvent } from 'react'
 
 import logoImage from '../../assets/logo.svg'
+import api from '../../services/api'
 
 import * as S from './styled'
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImage} alt="Github explorer" />
-    <S.Title>Explore repositórios no Github</S.Title>
+interface Repository {
+  full_name: string
+  description: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
 
-    <S.Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button>Pesquisar</button>
-    </S.Form>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
-    <S.Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/50281523?s=460&u=ff701a533e123e47961d0f2cf81b4081990c9d38&v=4"
-          alt="felipesuri"
-        />
-        <div>
-          <strong>felipesuri/felipesuri.com</strong>
-          <p>My personal website</p>
-        </div>
-        <S.Arrow />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/50281523?s=460&u=ff701a533e123e47961d0f2cf81b4081990c9d38&v=4"
-          alt="felipesuri"
-        />
-        <div>
-          <strong>felipesuri/felipesuri.com</strong>
-          <p>My personal website</p>
-        </div>
-        <S.Arrow />
-      </a>
-    </S.Repositories>
-  </>
-)
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const response = await api.get<Repository>(`/repos/${newRepo}`)
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+    setNewRepo('')
+  }
+
+  return (
+    <>
+      <img src={logoImage} alt="Github explorer" />
+      <S.Title>Explore repositórios no Github</S.Title>
+
+      <S.Form onSubmit={handleAddRepository} >
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
+        <button>Pesquisar</button>
+      </S.Form>
+
+      <S.Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <S.Arrow />
+          </a>
+        ))}
+      </S.Repositories>
+    </>
+  )
+}
 
 export default Dashboard
